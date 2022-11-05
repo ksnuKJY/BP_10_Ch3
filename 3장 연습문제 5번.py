bbb9ea265aeb6da24ae51b2efc381986514e19ae
jinyong <112368808+ksnuKJY@users.noreply.github.com>
2022-11-05 22:01:39 +0900

    Add files via upload

---
+++ 3장 연습문제 5번.py
@@ -0,0 +1,12 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t=turtle.Turtle()
+>>> t.shape("turtle")
+>>> t.lt(45)
+>>> t.fd(141)
+>>> t.setheading(0)
+>>> t.goto(0,0)
+>>> t.fd(100)
+>>> t.lt(90)
+>>> t.fd(100)
